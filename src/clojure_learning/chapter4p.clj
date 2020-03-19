2709f31dbf4b2b13391de40bebcf2639a1e29c36
Paula ASto <carolinaasto@gmail.com>
2020-03-19 10:57:56 -0500

    Exercises solved chapter 4

--- src/clojure_learning/chapter4p.clj
+++ src/clojure_learning/chapter4p.clj
@@ -53,3 +53,71 @@
 ;; => ({:name "Edward Cullen", :glitter-index 10}
 ;;     {:name "Jacob Black", :glitter-index 3}
 ;;     {:name "Carlisle Cullen", :glitter-index 6})
+
+(def suspects-list (glitter-filter 3 (mapify (parse (slurp filename)))))
+
+
+;; --------------------------------------------
+;; The vampire analysis program you now have is already decades ahead of anything else on 
+;; the market. But how could you make it better? I suggest trying the following:
+
+;; Exercise 1
+;; Turn the result of your glitter filter into a list of names.
+
+(defn list-names
+  [suspects]
+  (map #(% :name) suspects))
+
+(list-names suspects-list)
+;; => ("Edward Cullen" "Jacob Black" "Carlisle Cullen")
+
+(map :name suspects-list) ; another way
+;; => ("Edward Cullen" "Jacob Black" "Carlisle Cullen")
+
+
+;; Exercise 2
+;; Write a function, append, which will append a new suspect to your list of suspects.
+
+(defn append
+  [suspects new-suspect]
+  (conj suspects new-suspect))
+(def suspects-list (append suspects-list {:name "Pau" :glitter-index 13}))
+;; => ({:name "Pau", :glitter-index 13}
+;;     {:name "Edward Cullen", :glitter-index 10}
+;;     {:name "Jacob Black", :glitter-index 3}
+;;     {:name "Carlisle Cullen", :glitter-index 6})
+
+
+;; Exercise 3
+;; Write a function, validate, which will check that :name and :glitter-index are present when you append. 
+;; The validate function should accept two arguments: a map of keywords to validating functions, 
+;; similar to conversions, and the record to be validated.
+
+(defn validate
+  [new-map]
+  (and (contains? new-map :name) (contains? new-map :glitter-index))
+)
+
+(defn append-val
+  [suspects new-suspect]
+  (if (validate new-suspect)
+      (conj suspects new-suspect)
+      suspects))
+(def suspects-list (append-val suspects-list {:name "Caro" :glitter-index 20}))
+;; => ({:name "Caro", :glitter-index 20}
+;;     {:name "Edward Cullen", :glitter-index 10}
+;;     {:name "Jacob Black", :glitter-index 3}
+;;     {:name "Carlisle Cullen", :glitter-index 6})
+
+
+;; Exercise 4
+;; Write a function that will take your list of maps and convert it back to a CSV string. 
+;; You’ll need to use the clojure.string/join function.
+(defn parse-revert
+  "Convert map to filename"
+  [string]
+  (clojure.string/join "\n" (map #(clojure.string/join "," [(:name %) (:glitter-index %)]) string))
+  )
+
+(parse-revert suspects-list)
+;; => "Caro,20\nPau,13\nEdward Cullen,10\nJacob Black,3\nCarlisle Cullen,6"
